c1b3cdd426f4e711adfb226b70823fc40af9cec2
Andrew Gettz <drews_computer@Andrews-MacBook-Pro.local>
2022-07-02 10:15:03 -0400

    updated link between js files, pulled through specific issues and added error catches

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -63,8 +63,9 @@ const displayRepos = function(repos, searchTerm) {
         const repoName = repos[i].owner.login + "/" + repos[i].name;
 
     //create a container for each repo
-    const repoEl = document.createElement("div");
+    const repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center"; 
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     //create a span element to hold repository name 
     let titleEl = document.createElement("span");
